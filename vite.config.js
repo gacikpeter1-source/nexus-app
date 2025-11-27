677b6c02d8ad4255fb0dc16cb90b56ed87862128
Peter Gacik <gacikpeter1@gmail.com>
2025-11-27 20:26:11 +0100

    fix: Configure Vite for Firebase compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth', 'firebase/firestore']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/firebase/, /node_modules/]
+    }
   }
 })
