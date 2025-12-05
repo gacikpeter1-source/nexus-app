93f780af9264cd361d1b1b9c78b959f825ba63b4
Peter Gacik <gacikpeter1@gmail.com>
2025-12-05 22:23:43 +0100

    Update 3

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.esodof5osb"
+    "revision": "0.nughu45dcso"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/firebase/notificationSettings.js
+++ src/firebase/notificationSettings.js
@@ -240,3 +240,4 @@ export const areNotificationsEnabled = async (clubId, teamId, type) => {
     return false;
   }
 };
+
