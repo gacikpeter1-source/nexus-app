a9200e0f8e945b5440df9386b3c44593bd620ba3
Peter Gacik <gacikpeter1@gmail.com>
2025-12-16 21:53:08 +0100

    Update clubManagement

--- src/pages/ClubManagement.jsx
+++ src/pages/ClubManagement.jsx
@@ -8,7 +8,8 @@ import NotificationsTab from '../components/NotificationsTab';
 import { 
   getUserClubs, 
   getClub, 
-  getAllUsers, 
+  getAllUsers,
+  getUser, 
   updateClub, 
   updateUser, 
   getPendingRequests, 
