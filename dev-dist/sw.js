968bc366ca66910e53db691ac1a5490c804018db
Peter Gacik <gacikpeter1@gmail.com>
2025-12-18 08:10:55 +0100

    Rebuild notifications

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i0je5vlgcq4"
+    "revision": "0.n7vj4qpsr74"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
